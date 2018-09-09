b65b9054dc5a0141d8eccedefe6b59d33e9100f9
Abdullah Abuzaid <42889757+AAbuzaid@users.noreply.github.com>
2018-09-09 15:57:45 +0300

    Pong final

--- Pong.py
+++ Pong.py
@@ -24,7 +24,7 @@ score1 = 0
 score2 = 0
 
 
-# initialize ball_pos and ball_vel for a new ball in middle of table
+# initialize ball_pos and ball_vel for a new ball in middle of table 
 # if direction is RIGHT, the ball's velocity is upper right, else upper left
 def spawn_ball(direction):
     global ball_pos, ball_vel # these are vectors stored as lists
